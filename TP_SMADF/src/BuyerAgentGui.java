c086376a9cc14490599ac66705e6a7153a72029a
sokainadaabal <48890714+sokainadaabal@users.noreply.github.com>
2023-05-12 17:24:00 +0100

    service entre les agents

---
+++ TP_SMADF/src/BuyerAgentGui.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BuyerAgentGui  extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
